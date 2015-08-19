df12c7ed70e50da46c93e39f81b3d42b246f01a6
Irfan Radzi <irfan@spreeo.com>
2015-08-19 18:52:13 +0800

    initial value

--- bot/server.js
+++ bot/server.js
@@ -1,10 +1,27 @@
-var bot = require('vow-telegram-bot')
+var _bot = require('vow-telegram-bot'),
+	bot = new _bot({
+		token: '82257237:AAFQx9upOKq9qPIKqEfm4w1pz3LONGsCFMQ',
+		polling: {
+			timeout: 3,
+			limit: 100
+		}
+	})
 
-telegramBot.on('message', function(data) {
+var randomResponse = [
+	'Saya masih malu-malu dan belum bersedia lagi. Tunggu la dulu ye...',
+	'Ish awak ni. Saya belum siap lagi..',
+	'Heyyy!! Belum la! Cium baru tau.',
+	'Hihihi (tutup mulut malu-malu)',
+	'Awak ni la. Kan saya kata belum siap. Tak faham lagi ke?',
+]
+
+var i = Math.floor(4*Math.random());
+
+bot.on('message', function(data) {
 	if (data.text.charAt(0) === '/') {
-		telegramBot.sendMessage({
+		bot.sendMessage({
 			chat_id: data.chat.id,
-			text: 'Saya masih malu-malu dan belum bersedia lagi. Tunggu la dulu ye...'
+			text: randomResponse[i]
 		})
 	}
 })
