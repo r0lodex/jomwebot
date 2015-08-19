593dcbf68d6ef02e905816a9e59545781036812f
Irfan Radzi <irfan@spreeo.com>
2015-08-19 18:39:24 +0800

    check for / to reply so it won't pollute group chats

--- bot/server.js
+++ bot/server.js
@@ -1,8 +1,10 @@
 var bot = require('vow-telegram-bot')
 
 telegramBot.on('message', function(data) {
-	telegramBot.sendMessage({
-		chat_id: data.chat.id,
-		text: 'Saya masih malu-malu dan belum bersedia lagi. Tunggu la dulu ye...'
-	})
+	if (data.text.charAt(0) === '/') {
+		telegramBot.sendMessage({
+			chat_id: data.chat.id,
+			text: 'Saya masih malu-malu dan belum bersedia lagi. Tunggu la dulu ye...'
+		})
+	}
 })
