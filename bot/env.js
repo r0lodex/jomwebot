73fd10937f7c60368f96f7e0c2e91907233242a6
Irfan Radzi <irfan@spreeo.com>
2015-08-25 13:31:13 +0800

    Added sample env.js

---
+++ bot/env.js
@@ -0,0 +1,2 @@
+process.env['BOT_TOKEN'] = "12345678:AbCDEFG123poidlkj123"; // Your bot_token acquired from botfather
+process.env['JWJ_ID'] = "-123456789"; // Currently not in use — It should present the group_id
